509def0e531e875e4022014eab9d6ede6f3dc9a0
gaelLara21 <gaellara19@gmail.com>
2022-11-23 23:28:43 -0600

    Actualizar Datos

--- firebase.js
+++ firebase.js
@@ -28,4 +28,8 @@
 
     export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
 
-    
+    export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+    export const updateTask = (id, newFields) => updateDoc(doc(db, "tasks", id), newFields);
+
+
